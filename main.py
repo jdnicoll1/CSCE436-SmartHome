6d0e9438c1ab805b9b50d32ef12cbe7052884b6d
jdnicoll1 <jdnicoll1@gmail.com>
2022-11-21 18:44:46 -0600

    data points showing on hands

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import cv2
+import mediapipe as mp
 import time
 
 cap = cv2.VideoCapture(0)
 
+mpHands = mp.solutions.hands 
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+
+
 while True:
     success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    
+    if(results.multi_hand_landmarks): 
+        for handLandmarks in results.multi_hand_landmarks: 
+            mpDraw.draw_landmarks(img, handLandmarks, mpHands.HAND_CONNECTIONS)
 
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    img_invert = cv2.flip(img, 1) #people are more used to inverted image
+    cv2.imshow("Image", img_invert)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'): #press q to quit
+        break
